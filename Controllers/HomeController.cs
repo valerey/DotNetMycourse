2878c98b7e4e69fe7e5bb5c6cae5324723e1ec3f
Valentina Reyna <valerey19@gmail.com>
2020-04-21 18:45:00 +0200

    aggiunto Mvc

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyCourse.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Sono la Index Della home");         
+        }
+        
+    }
+}
